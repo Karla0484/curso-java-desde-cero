303c9fb75db937ab614f92e1d35841b7a54e2d18
Karla0484 <109536801+Karla0484@users.noreply.github.com>
2026-02-17 21:25:25 -0600

    Add files via upload

---
+++ curso_java/dia02/Variables.java
@@ -0,0 +1,11 @@
+package dia02;
+
+public class Variables {
+    static int edad = 30;
+    static String nombre = "Alberto";
+    static double salario = 15000.50;
+    
+    public static void main(String[] args) {
+        System.out.println("Mi edad es " + edad);
+    }
+}
